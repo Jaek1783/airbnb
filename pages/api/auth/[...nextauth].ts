021cef3fc7e62e02ae6a176186673c84cf1c18b2
jaekeun Choi <jaek1783@gmail.com>
2023-08-31 13:56:27 +0900

    prisma설치 및 연결, mongoDB 설치 및 연결, jwt를 이용한 토큰생성, 회원가입 구현

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth, { SessionOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credential';
+import prisma from '@/app/libs/prismadb';
+import bcrypt from 'bcrypt';
+
+export const authOptions: SessionOptions = {
+  adapter: new PrismaAdapter({ prisma }), // Fix here
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' }, // Fix here
+      },
+      async authorize(credentials) { // Fix here
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/',
+  },
+  debug: process.env.NODE_ENV === 'development', // Fix here
+  session: {
+    strategy: 'jwt', // Fix here
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);

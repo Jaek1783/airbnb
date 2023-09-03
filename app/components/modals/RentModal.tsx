06445b9fb482ced9f31f746e8e0ca2d4f34f055f
jaekeun Choi <jaek1783@gmail.com>
2023-09-03 15:45:27 +0900

    지도 api컴포넌트 연결

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,8 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -46,7 +48,11 @@ const RentModal = () => {
     })
 
     const category = watch('category');
+    const location = watch('location');
 
+    const Map = useMemo(()=>dynamic(()=>import('../Map'),{
+        ssr:false
+    }),[location])
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
             shouldValidate : true,
@@ -114,7 +120,13 @@ const RentModal = () => {
                     title="Where is your place Located?"
                     subtitle="Help guests find you"
                 />
-                <CountrySelect/>
+                <CountrySelect
+                     value={location} 
+                     onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map
+                    center={location?.latlng}
+                />
             </div>
         )
     }

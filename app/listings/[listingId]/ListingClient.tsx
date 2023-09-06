f112befb4d275784abdc2c68ebf926ba3afec063
jaekeun Choi <jaek1783@gmail.com>
2023-09-06 21:35:22 +0900

    detailPage마감, 서버연결, 전송 후 캘린더 적용

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -3,7 +3,7 @@ import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
-import { SafeListings, SafeUser } from "@/app/types";
+import { SafeListings, SafeReservation, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
@@ -22,7 +22,7 @@ const initialDateRange = {
 }
 
 interface ListingClientProps {
-    reservations?:Reservation[];
+    reservations?:SafeReservation[];
     listing: SafeListings & {
         user : SafeUser
     };
